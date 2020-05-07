b9dfb46c862e9c9f8064cd08447e83cbefa578ca
Your Name <you@example.com>
2020-05-07 19:57:50 -0300

    Agrego menu al proyecto

--- app/src/main/java/com/example/clase6/MainActivity.java
+++ app/src/main/java/com/example/clase6/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.clase6;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar toolbar = getSupportActionBar();
+        toolbar.setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menuItem){
+            super.getMenuInflater().inflate(R.menu.menu,menuItem);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        if(item.getItemId()==R.id.opcion1){
+            Log.d("Click en menu","Se hizo click en configuracion!!!");
+        }else if(item.getItemId()==R.id.opcion2){
+            Log.d("Click en menu","Se hizo click en ALTA");
+        }else if(android.R.id.home == item.getItemId()){
+            super.finish();
+        }
+
+        return  false;
     }
 }
